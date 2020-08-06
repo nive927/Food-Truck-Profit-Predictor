71c2f8e2b2af8b9ebf229189385f545e8005f9b9
Nivedhitha D <50760049+nive927@users.noreply.github.com>
2020-08-06 23:00:30 +0530

    Update computeCost.m

--- computeCost.m
+++ computeCost.m
@@ -6,10 +6,9 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% Need to return the following variables correctly 
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
@@ -17,8 +16,5 @@ predictions = X * theta;
 m = length(y);
 J = 1/(2*m) * sum((predictions - y) .^ 2);
 
-
-
-% =========================================================================
 
 end
